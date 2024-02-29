a447a8518c28509a82e69822c5708fc846fa7173
Ryan Cleven <rcleven@mac.com>
2024-02-28 23:52:35 -0800

    Added common root for build system

---
+++ lib/idl/BuildBase.hx
@@ -0,0 +1,111 @@
+package idl;
+import idl.Options;
+
+class BuildBase {
+	// Put any necessary includes in this string and they will be added to the generated files
+
+	var _options : Options;
+
+	public function new(options : Options) {
+		_options = options;
+	}
+
+	function getHLInclude() {
+		return "";
+	}
+	function getJVMInclude() {
+		return "";
+	}
+
+	function generate() {
+		_options.target =  switch (target) {
+			case "hl": idl.Options.Target.TargetHL;
+			case "jvm": idl.Options.Target.TargetJVM;
+			default: idl.Options.Target.TargetHL;
+		};
+		_options.includeCode = switch (target) {
+			case idl.Options.Target.TargetHL: getHLInclude();
+			case idl.Options.Target.TargetJVM: getJVMInclude();
+			default: "";
+		};
+
+		trace('Generating target ${target}');
+		idl.generator.Generate.generateCpp(_options);
+	}
+
+	var target = "hl";
+	var builder = "Ninja";
+	var arch = "x86_64";
+    var idlPath = "ext/hl-idl";
+    var config = "Debug";
+
+	var installDir = null;
+
+	function build() {
+		sys.FileSystem.createDirectory('build/${target}/${arch}/${config}');
+		sys.FileSystem.createDirectory(installDir);
+        var cmd = 'cmake -G"${builder}" -DTARGET_ARCH=${arch} -DPATH_TO_IDL=${idlPath} -DTARGET_HOST=${target} -DCMAKE_BUILD_TYPE=${config} -DCMAKE_INSTALL_PREFIX=${installDir} -B build/${target}/${arch}/${config}';
+        trace('$cmd');
+        Sys.command(cmd);
+
+        cmd = 'cmake --build build/${target}/${arch}/${config}';
+        trace('$cmd');
+        Sys.command(cmd);
+	}
+
+	function install() {
+
+		var cmd = 'cmake --install build/${target}/${arch}/${config}';
+		trace('$cmd');
+		Sys.command(cmd);
+	}
+
+	function parseArgs() {		
+		var args = Sys.args();
+
+		if (args.length > 0) {
+			var cmd = args.shift();
+
+            while (args.length > 0) {
+                var arg = args.shift();
+                switch (arg) {
+                    case "--target":
+                        target = args.shift();
+                    case "--builder":
+                        builder = args.shift();
+                    case "--arch":
+                        arch = args.shift();
+                    case "--idl":
+                        idlPath = args.shift();
+                    case "--config":
+                        config = args.shift();
+					case "--dir":
+						installDir = args.shift();
+                    default:
+                }
+            }
+			if (installDir == null) {
+				installDir = 'installed/${target}/${arch}/${config}';
+			}
+			switch (cmd) {
+				case "generate":
+					generate();
+				case "build":
+					build();
+				case "install":
+					install();
+				default:
+			}
+		} else {
+            trace("Usage: haxe config.hxml [generate|build|install] [options]");
+			trace("  generate: Generate the target code");
+			trace("  build: Build the target code");
+			trace("  install: Install the target code");
+			trace("  --target: The target platform (hl, jvm)");
+			trace("  --builder: The build system (Ninja, Make)");
+			trace("  --arch: The target architecture (x86_64, arm64)");
+			trace("  --idl: The path to the idl directory");
+			trace("  --config: The build configuration (Debug, Release)");
+        }
+	}
+}
